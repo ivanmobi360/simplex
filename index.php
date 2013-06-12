21f9b028bef611245b5ca96f920bf629599292d9
irodriguez <irodriguez@mobi360.ca>
2013-06-12 16:48:30 -0500

    using request, response

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-$input = $_GET['name'];
+require_once __DIR__ . '/autoload.php';
 
-printf('Hello %s', $input);
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new Response(
+        sprintf("Hello %s", htmlspecialchars($input, ENT_QUOTES, 'UTF-8') )
+        );
+$response->setMaxAge(10);
+$response->send();
+
+echo "Ip: " . $request->getClientIp();
+echo '<br><pre>'. $response;
