6de7494a286c6875276594bd551ae2f4dff0775a
irodriguez <irodriguez@mobi360.ca>
2013-06-13 10:36:12 -0500

    we routes now

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,1 @@
-<?php  $name = isset($name)? $name: 'World' ?>
-
 Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
--- web/front.php
+++ web/front.php
@@ -1,27 +1,29 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
 
-$map = array(
-        '/hello' => 'hello',
-        '/bye' => 'bye'
-        ); 
+$routes = include __DIR__ . '/../src/app.php';
 
-$path = $request->getPathInfo();
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-if(isset($map[$path])){
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include __DIR__ . '/../src/pages/' . $map[$path] . '.php' ;
+    include __DIR__ . '/../src/pages/' . $_route . '.php' ;
     $response = new Response(ob_get_clean());
-}else{
+} catch ( ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error ocurred', 500);
 }
-
 
 $response->send();
