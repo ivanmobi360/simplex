7f5f53ce7b25e5b0be9710000ac4d015c6f09c6d
irodriguez <irodriguez@mobi360.ca>
2013-06-13 11:30:00 -0500

    use a callback

--- src/app.php
+++ src/app.php
@@ -8,7 +8,9 @@ $routes = new RouteCollection();
 
 $routes->add('hello', new Route('/hello/{name}', array(
         'name'=>'World',
-        '_controller' => 'render_template'
+        '_controller' => function ($request){
+            return render_template($request);
+        }
         )));
 
 $routes->add('bye', new Route('/bye', array(
