ff4f47de7ff4c518aa9df0880298e75df67db14b
irodriguez <irodriguez@mobi360.ca>
2013-06-13 16:10:50 -0500

    begin tests

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -1,19 +1,19 @@
 <?php 
-
 namespace Simplex;
 
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
+
 
 class Framework
 {
     protected $matcher;
     protected $resolver;
     
-    public function __construct(UrlMatcher $matcher, ControllerResolver $resolver)
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $resolver)
     {
         $this->matcher = $matcher;
         $this->resolver = $resolver;
