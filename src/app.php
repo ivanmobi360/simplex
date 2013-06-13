ad3df6b272fa8b37faabf77540307c4253f7dd6e
irodriguez <irodriguez@mobi360.ca>
2013-06-13 11:48:38 -0500

    is_leap_year

--- src/app.php
+++ src/app.php
@@ -1,11 +1,21 @@
 <?php 
 
-use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-$routes = new RouteCollection();
+function is_leap_year($year = null){
+    if(null == $year){
+        $year = date('Y');
+    }
+    
+    return 0==$year%400 || ( 0==$year%4 && 0!=$year%100  );
+    
+}
 
+
+$routes = new RouteCollection();
 $routes->add('hello', new Route('/hello/{name}', array(
         'name'=>'World',
         '_controller' => function ($request){
@@ -15,6 +25,17 @@ $routes->add('hello', new Route('/hello/{name}', array(
 
 $routes->add('bye', new Route('/bye', array(
         '_controller' => 'render_template'
+        )));
+
+
+$routes->add('leap_year', new Route('/is_leap_year/{year}', array(
+        'year' => null,
+        '_controller' => function ($request){
+            if(is_leap_year($request->attributes->get('year'))){
+                return new Response('The year is leap');
+            }
+            return new Response('Nope, the year is not leap');
+        }
         )));
 
 return $routes;
