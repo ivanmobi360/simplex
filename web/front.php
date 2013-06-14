e7f573435b5f5b27e126711d0cf122b3e91ad67a
irodriguez <irodriguez@mobi360.ca>
2013-06-14 16:42:29 -0500

    using a dependency injection container now.

--- web/front.php
+++ web/front.php
@@ -1,31 +1,15 @@
 <?php
-use Simplex\StringResponseListener;
 
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
 
-use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
-
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-
-use Symfony\Component\HttpKernel\HttpCache\Store;
-
-use Symfony\Component\HttpKernel\HttpCache\HttpCache;
-
-use Simplex\ContentLengthListener;
-
-use Simplex\GoogleListener;
-
-use Symfony\Component\EventDispatcher\EventDispatcher;
-
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+//use Symfony\Component\HttpKernel\HttpCache\Store;
+//use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
 
 function render_template(Request $request)
 {
@@ -38,23 +22,8 @@ function render_template(Request $request)
 $request = Request::createFromGlobals();
 
 $routes = include __DIR__ . '/../src/app.php';
-
-$context = new RequestContext();
-$context->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
-$resolver = new ControllerResolver();
-
-$dispatcher = new EventDispatcher();
-//$dispatcher->addSubscriber(new GoogleListener());
-$dispatcher->addSubscriber(new ContentLengthListener());
-$dispatcher->addSubscriber(new RouterListener($matcher));
-$dispatcher->addSubscriber(new ExceptionListener('Calendar\\Controller\\ErrorController::exceptionAction'));
-$dispatcher->addSubscriber(new ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new StringResponseListener());
-
-
-$framework = new \Simplex\Framework($dispatcher, $resolver);
-$framework = new HttpCache($framework, new Store(__DIR__ .'/../cache'));
-$response = $framework->handle($request);
+$sc = include __DIR__ . '/../src/container.php';
+//$framework = new HttpCache($framework, new Store(__DIR__ .'/../cache'));
+$response = $sc->get('framework')->handle($request);
 
 $response->send();
